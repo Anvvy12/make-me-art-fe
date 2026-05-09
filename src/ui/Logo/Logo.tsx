e3007691b4b4860824f2da1a43a12a3d8cf9af91
Bogdan <romanbs.wo@gmail.com>
2026-05-09 16:35:08 +0300

    add rest client

--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -1,7 +1,14 @@
 import LogoImg from 'img/Logo.png';
 
 import s from './Logo.module.scss';
+import { Link } from 'react-router-dom';
+import { NAVIGATION_LINKS } from '../../constants/SECTIONS_CONSTANTS';
+import cn from 'classnames';
 
 export default function Logo() {
-  return <img className={s.Logo} src={LogoImg} alt={'logo'} />;
+  return (
+    <Link className={cn(s.link, s.Logo)} to={NAVIGATION_LINKS.MAIN}>
+      <img className={s.img} src={LogoImg} alt={'logo'} />
+    </Link>
+  );
 }
