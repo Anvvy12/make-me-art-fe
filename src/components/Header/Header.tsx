464820895363cffa7c06635cc4e23ec7c36be2d5
Bogdan <romanbs.wo@gmail.com>
2026-05-12 13:21:37 +0300

    init gh-page branch

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,22 @@ import Logo from 'ui/Logo';
 import BurgerMenu from './ui/BurgerMenu';
 import { NAVIGATION_LINKS } from 'constants/SECTIONS_CONSTANTS';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import LanguageSelect from './ui/LanguageSelect';
 import Button from 'ui/Button';
+import ArrowIcon from 'svg/arrow.svg?react';
 
 export default function Header() {
+  const n = useNavigate();
+
   const { t } = useTranslation(undefined, { keyPrefix: 'common' });
 
   return (
     <section className={cn(s.Header)}>
       <div className={s.inner}>
-        <Button className={s.backButton}>{'<'}</Button>
+        <Button className={s.backButton} onClick={() => n(-1)}>
+          <ArrowIcon className={s.arrowIcon} />
+        </Button>
         <Logo />
         <nav className={s.nav}>
           <Link className={s.link} to={NAVIGATION_LINKS.MAIN}>
