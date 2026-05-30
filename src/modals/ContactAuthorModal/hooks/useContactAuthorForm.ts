28497b4d8db7404074b32acacbaa230634c8cd50
Bogdan <romanbs.wo@gmail.com>
2026-05-30 12:49:07 +0300

    MKS-14 email sent functionality

--- src/modals/ContactAuthorModal/hooks/useContactAuthorForm.ts
+++ src/modals/ContactAuthorModal/hooks/useContactAuthorForm.ts
@@ -36,6 +36,7 @@ export function useContactAuthorForm({ artworkTitle, onSuccess }: TArgs) {
       message: yup.string().trim().required(t('message_required')),
     }),
     onSubmit: async (values, { resetForm, setSubmitting }) => {
+      console.log('Sending email:', values);
       try {
         await emailjs.send(
           import.meta.env.VITE_EMAILJS_SERVICE_ID,
